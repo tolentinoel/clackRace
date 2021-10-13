204277c1799f2f11c36da224450d9ceb31d714c4
tolentinoel <rckrleyn@gmail.com>
2021-10-13 16:56:17 -0700

    initialize WordContainer

--- clacktime/src/App.js
+++ clacktime/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Topnav from './components/Topnav';
+import WordContainer from './components/WordContainer';
 
 function App() {
   return (
     <div className="App">
-      
       <Topnav/>
+      <WordContainer/>
     </div>
   );
 }
---
+++ clacktime/src/components/WordContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/WordContainer.css';
+
+class WordContainer extends Component {
+    render() {
+        return (
+            <div className="wordCDiv">
+
+            </div>
+        );
+    }
+}
+
+export default WordContainer;
