9d927a27892efdefda713094cf569e9824ac8412
tolentinoel <rckrleyn@gmail.com>
2021-10-13 18:06:22 -0700

    Window added with test ipsum, Form in place with onChange function

--- clacktime/src/components/WordContainer.js
+++ clacktime/src/components/WordContainer.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import '../styles/WordContainer.css';
 
 class WordContainer extends Component {
+
+    state = {
+        text: ""
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
     render() {
         return (
             <div className="wordCDiv">
-
+                <div id="window">
+                    <p>Bacon ipsum dolor amet jowl ground round tri-tip hamburger capicola kielbasa prosciutto. Ham sirloin filet mignon, ground round brisket ball tip bresaola shoulder venison. Filet mignon drumstick prosciutto, ground round beef ribs tenderloin bresaola chuck bacon ham boudin rump pancetta. Ground round t-bone turkey shank jowl turducken cow salami porchetta leberkas tenderloin meatloaf. Tongue pig swine pork, meatball jerky short loin salami turducken pork loin burgdoggen short ribs chislic filet mignon.</p>
+                </div>
+                <form id="form">
+                    <input
+                        className="form-control"
+                        type="text"
+                        placeholder="Type here"
+                        value={this.state.text}
+                        name="text"
+                        onChange= {this.handleChange}
+                    />
+                </form>
             </div>
         );
     }
