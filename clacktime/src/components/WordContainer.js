8249298fa21a7aed049a69888b92fc2bf85a7f30
tolentinoel <rckrleyn@gmail.com>
2021-10-21 21:29:41 -0700

    revising handleChange to trigger score render

--- clacktime/src/components/WordContainer.js
+++ clacktime/src/components/WordContainer.js
@@ -4,29 +4,53 @@ import '../styles/WordContainer.css';
 class WordContainer extends Component {
 
     state = {
+        ipsum: "Bacon ipsum dolor amet jowl ground round tri-tip hamburger capicola kielbasa prosciutto. Ham sirloin filet mignon, ground round brisket ball tip bresaola shoulder venison. Filet mignon drumstick prosciutto, ground round beef ribs tenderloin bresaola chuck bacon ham boudin rump pancetta. Ground round t-bone turkey shank jowl turducken cow salami porchetta leberkas tenderloin meatloaf. Tongue pig swine pork, meatball jerky short loin salami turducken pork loin burgdoggen short ribs chislic filet mignon.",
         text: "",
-        wordPool: []
+        wordPool: [],
+        score: 0
     }
 
     handleChange = (e) => {
 
         this.setState({[e.target.name]: e.target.value,})
 
+        // if text has space, set state
         if (e.target.value.includes(" ")) {
-            this.setState({
-                text: "",
-                wordPool: `${this.state.wordPool +" "+ this.state.text}`
-            })
+            debugger
+            if (e.target.value === this.state.wordPool[this.state.wordPool.length-1]){
+
+                this.setState({
+                    text: "",
+                    wordPool: `${this.state.wordPool +" "+ this.state.text}`
+                })
+            }
 
         }
     }
 
+
+    renderScore = () => {
+        debugger
+        let str = this.state.wordPool
+        
+        if (this.state.text === str.split(" ")[str.length-1]) {
+            let num = this.state.score + 1
+            this.setState({
+                score: num
+            })
+        }
+    }
+
     render() {
-        console.log(this.state.wordPool )
+        
+     
         return (
             <div className="wordCDiv">
+                <p>{this.state.wordPool}</p>
+              
+                <p>{this.state.score}</p>
                 <div id="window">
-                    <p>Bacon ipsum dolor amet jowl ground round tri-tip hamburger capicola kielbasa prosciutto. Ham sirloin filet mignon, ground round brisket ball tip bresaola shoulder venison. Filet mignon drumstick prosciutto, ground round beef ribs tenderloin bresaola chuck bacon ham boudin rump pancetta. Ground round t-bone turkey shank jowl turducken cow salami porchetta leberkas tenderloin meatloaf. Tongue pig swine pork, meatball jerky short loin salami turducken pork loin burgdoggen short ribs chislic filet mignon.</p>
+                    <p>{this.state.ipsum}</p>
                 </div>
                 <form id="form">
                     <input
